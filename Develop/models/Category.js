45b16bd22fdde161ed29fc6b2798628cf1c09784
Matthew Swineford <m.swineford@yahoo.com>
2022-11-14 12:08:18 -0500

    finished the database models

--- Develop/models/Category.js
+++ Develop/models/Category.js
@@ -7,6 +7,16 @@ class Category extends Model {}
 Category.init(
   {
     // define columns
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      prinaryKey: true,
+      autoIncrement: true
+    },
+    catagory_name: {
+      type: DataTypes.STRING,
+      allowNull: false
+    }
   },
   {
     sequelize,
