88e4bedb20d73c47d86e4cab87ce1021916fa751
Matthew Swineford <m.swineford@yahoo.com>
2022-11-15 21:13:17 -0500

    worked on the routes and models index

--- Develop/models/index.js
+++ Develop/models/index.js
@@ -5,12 +5,26 @@ const Tag = require('./Tag');
 const ProductTag = require('./ProductTag');
 
 // Products belongsTo Category
+Product.belongsTo(Category, {
+  forieginKey: "product_id"
+});
 
 // Categories have many Products
+Category.hasMany(Product, {
+  foreignKey: "category_id"
+});
 
 // Products belongToMany Tags (through ProductTag)
+Product.belongsToMany(Tag, {
+  through: ProductTag,
+  foreignKey: "product_id"
+});
 
 // Tags belongToMany Products (through ProductTag)
+Tag.belongsToMany(Product, {
+  through: ProductTag,
+  foriegnKey: "product_id"
+});
 
 module.exports = {
   Product,
